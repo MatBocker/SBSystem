a7f72e8b4eae24bbae4da15c7f997981708dc0e7
MatBocker <mat.bocker@gmail.com>
2021-03-16 14:58:43 -0300

    doner

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,6 @@ Route::resource('treinos','TreinosController');
 Route::get('desativadosA', 'AlunosController@desativadosA')->name('alunos.desativadosA');
 Route::patch('alunos/restore/{id}', 'AlunosController@restore')->name('alunos.restore');
 Route::get('cadastrar', 'AlunosController@cadastrar')->name('alunos.cadastrar');
-Route::resource('planos','PlanosController');
 Route::get('desativados', 'PlanosController@desativados')->name('planos.desativados');
 Route::patch('planos/restore/{id}', 'PlanosController@restore')->name('planos.restore');
 Route::get('alunos/{aluno}/ava', 'AlunosController@ava')->name('alunos.ava');
@@ -35,4 +34,9 @@ Route::get('alunos/{aluno}/ultimaAva', 'AlunosController@ultimaAva')->name('alun
 Route::get('alunos/{aluno}/mostraAva', 'AlunosController@mostraAva')->name('alunos.mostraAva');
 Route::get('alunos/{aluno}/criarAva', 'AlunosController@criarAva')->name('alunos.criarAva');
 Route::put('alunos/avaliacaoCriar/{id}', 'AlunosController@avaliacaoCriar')->name('alunos.avaliacaoCriar');
+});
+
+Route::middleware(['auth','dono'])->group(function() {
+    Route::resource('planos','PlanosController');
+
 });
